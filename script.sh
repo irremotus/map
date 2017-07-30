1eb764b4624b77f1826d76721fbc85d61659f341
Kevin Schmittle <kschmittle@cs.nmt.edu>
2017-07-30 14:59:07 -0600

    vars in env, rm output files before run, and output to *_new_map.txt

--- script.sh
+++ script.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-MAP_FOLDER="path of the map files"
-FASTQ_FOLDER="path of the fastq files"
+##########################################
+# Set MAP_FOLDER and FASTQ_FOLDER in env #
+##########################################
+
+if [ -z "$MAP_FOLDER" ]; then
+	echo "Need MAP_FOLDER"
+	exit 1
+fi
+if [ -z "$FASTQ_FOLDER" ]; then
+	echo "Need FASTQ_FOLDER"
+	exit 1
+fi
 
 
+rm ${MAP_FOLDER}/*_new_map.txt
 for f in $(ls ${MAP_FOLDER}/*_map.txt); do
-	F=$(basename $f); awk -v DIR=${FASTQ_FOLDER}/Group_${F%%_*}_fastq -f script.awk $f > ${f}.new
+	F=$(basename $f); awk -v DIR=${FASTQ_FOLDER}/Group_${F%%_*}_fastq -f script.awk $f > ${MAP_FOLDER}/${F%_map.txt}_new_map.txt
 done
